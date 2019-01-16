716eb72ae6d599a1b5b0e06f9dbab16ff26773cc
BaseChip <info@falkmichaelis.eu>
2019-01-16 14:49:47 +0100

    Revert "Add files via upload (#4)"
    This reverts commit fe5d304ff4d24f3bf2becb5330e02f85f18e372d.

--- src/main/java/de/thebotdev/rulesbot/util/RegisterListener.java
+++
@@ -1,8 +0,0 @@
-package de.thebotdev.rulesbot.util;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.RUNTIME)
-public @interface RegisterListener {
-}
